9fd0f7c047de0f1663db73232d5fc45b104c00e8
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-19 07:28:52 -0400

    Create adjacentFloats.jl

---
+++ src/adjacentFloats.jl
@@ -0,0 +1,57 @@
+#=
+  These routines return +/-Inf when given +/-Inf.
+  That differs from nextfloat(-Inf) == -realmax(), prevfloat(Inf) == realmax()
+  (prevfloat(Inf)==Inf makes more sense to me, and likely is more helpful).
+  And steps twice when given values of very small magnitude (see paper).
+  The alternative implementation, converting to [U]Int and adding/subtracting 1,
+  returns NaN when given +/-Inf -- and checking for Inf adds branching. 
+  ref:
+  'Predecessor and Successor In Rounding To Nearest' by Siegried M. Rump
+  "The routines deliver the exact answer except for a small range near underflow,
+   in which case the true result is overestimated by eta [the value added/subtracted below]."
+=#
+# exact for |x| > 8.900295434028806e-308
+nextNearerToZero(x::Float64)   = (0.9999999999999999*x)-5.0e-324 # (x-1.1102230246251568e-16*x)-5.0e-324 
+nextAwayFromZero(x::Float64)   = (1.0000000000000002*x)+5.0e-324 # (x+1.1102230246251568e-16*x)+5.0e-324
+# exact for |x| > 4.814825f-35
+nextNearerToZero(x::Float32)   = (0.99999994f0*x)-1.435f-42      # (x-5.960465f-8*x)-1.435f-42
+nextAwayFromZero(x::Float32)   = (1.00000010f0*x)+1.435f-42      # (x+5.960465f-8*x)+1.435f-42
+# the multiplicative formulation for Float16 is exact for |x| > Float16(0.25)
+# which is quite coarse, we do not use that here
+nextNearerToZero(x::Float16) = (x < 0) ? nextfloat(x) : prevfloat(x)
+nextAwayFromZero(x::Float16) = (x < 0) ? prevfloat(x) : nextfloat(x)
+
+@inline nextFloat{T<:AbstractFloat}(x::T) = signbit(x) ? nextNearerToZero(x) : nextAwayFromZero(x)
+@inline prevFloat{T<:AbstractFloat}(x::T) = signbit(x) ? nextAwayFromZero(x) : nextNearerToZero(x)
+
+
+function nextFloat{T<:AbstractFloat}(x::T, n::Int)
+    for i in 1:n
+        x = nextFloat(x)
+    end
+    x
+end
+
+function prevFloat{T<:AbstractFloat}(x::T, n::Int)
+    for i in 1:n
+        x = prevFloat(x)
+    end
+    x
+end
+
+function nFloatsSeparating{T<:AbstractFloat}(a::T, b::T)
+    isneg = (a > b)
+    a,b = minmax(a,b)
+    
+    fra,xpa = frexp(a)
+    frb,xpb = frexp(b)
+    z =
+       if xpa==xpb
+          floor(Int64,((y-x)/(2^xbp))*4503599627370496) # *2^52
+       elseif xpa+1 == xpb
+          4503599627370496
+       else
+          4503599627370496
+       end
+    isneg ? -z : z
+end
